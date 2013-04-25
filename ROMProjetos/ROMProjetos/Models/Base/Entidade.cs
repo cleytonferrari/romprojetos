2b9ae388e77cc70a27947377508ac35d6b130d75
Cleyton Ferrari <cleyton@w7br.com>
2013-04-25 17:48:30 -0400

    Excluidos alguns arquivos

--- ROMProjetos/ROMProjetos/Models/Base/Entidade.cs
+++ ROMProjetos/ROMProjetos/Models/Base/Entidade.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.IdGenerators;
 
 namespace ROMProjetos.Models.Base
 {
-    public class Entidade: VerificaErros
+    public class Entidade : VerificaErros
     {
         [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
         public string Id { get; set; }
